68d5ab157a078c81a002a5324de54c82ec235f09
Sglass03 <shaun.glassman@gmail.com>
2021-07-25 09:47:54 -0400

    Added os library

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
 
 # database_path = "postgresql://postgres:root@localhost:5432/capstone_test"
 database_path = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
