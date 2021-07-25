6e504cfb463d671e1f69d4b6a19688a33df33cf1
Sglass03 <shaun.glassman@gmail.com>
2021-07-25 09:45:52 -0400

    Switched database path for deployment

--- models.py
+++ models.py
@@ -1,8 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
-database_path = "postgresql://postgres:root@localhost:5432/capstone_test"
-# database_path = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
+# database_path = "postgresql://postgres:root@localhost:5432/capstone_test"
+database_path = os.environ.get('DATABASE_URL').replace("://", "ql://", 1)
 
 db = SQLAlchemy()
 
